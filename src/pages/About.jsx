bf7e3534c4fb104f99952616c5444d46a59d6497
Tantos Alexandra <alexandra.tantos@yahoo.com>
2024-03-17 21:11:38 +0200

    commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
           </div>
         </div>
       </div>
-      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
         At ComfyStore, we understand the importance of creating a cozy and
         inviting atmosphere in your home. That's why we've curated a stunning
         collection of furniture that combines style, quality, and comfort, all
@@ -23,7 +23,7 @@ const About = () => {
         living room, upgrade your bedroom, or transform your dining area, we
         have everything you need to bring your vision to life.{" "}
       </p>
-      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
         What sets ComfyStore apart is our dedication to offering top-notch
         craftsmanship and premium materials without compromising on
         affordability. We partner with reputable manufacturers to ensure that
@@ -33,7 +33,7 @@ const About = () => {
         extensive catalog with ease, and our helpful filters make it simple to
         narrow down your options based on your preferences and needs.
       </p>
-      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
         But our dedication to comfort goes beyond just the furniture itself. We
         also offer a wide selection of accessories and soft furnishings,
         including plush cushions, cozy throws, stylish rugs, and more, to add
@@ -42,7 +42,7 @@ const About = () => {
         we offer flexible payment options and hassle-free returns, so you can
         shop with confidence knowing that your satisfaction is our top priority.
       </p>
-      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
         Plus, with secure checkout and fast shipping, you can have your new
         furniture delivered straight to your doorstep in no time. But our
         commitment to your satisfaction doesn't end there. Our knowledgeable
@@ -50,7 +50,7 @@ const About = () => {
         or concerns, ensuring that your shopping experience with us is nothing
         short of exceptional.
       </p>
-      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto">
+      <p className=" mt-6 text-lg leading-8 max-w-2xl mx-auto text-justify">
         {" "}
         So why wait? Discover the joy of comfortable living with ComfyStore
         today. Start browsing our collection and transform your home into the
